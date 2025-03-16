d96c72615f96d38b2b5607d88b00e40707eeb7e8
riefproject <riefedu@gmail.com>
2025-03-17 03:29:40 +0700

    cherry-pick preparation

--- include/raffi.h
+++ include/raffi.h
@@ -1,6 +1,21 @@
 #ifndef RAFFI_H
 #define RAFFI_H
 
+//Mencari nilai terbesar antara skor player dan HiScores
+int getMax(HiScore scores[], Game* game, GameResources* resources);
 
+//Mengambil data dari db
+void loadHiScores(HiScore scores[]);
 
+//Menyimpan data ke db
+void saveHiScores(HiScore scores[]);
+
+//Menyimpan perubahan
+void updateHighScore(Game* game, GameResources* resources);
+
+//Aturan skor
+void addScore(Game* game, int row);
+
+//Getter score player
+long long int playerScore(Game* game);
 #endif
