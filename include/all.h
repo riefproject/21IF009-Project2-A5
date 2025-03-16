79d14cba3c4531aaa857ed131ae8a1bbe0de83c5
riefproject <riefedu@gmail.com>
2025-03-17 03:07:41 +0700

    Add falling blocks logic, defines.h for game constants and structures; update all.h with necessary headers

--- include/all.h
+++ include/all.h
@@ -1,6 +1,28 @@
 #ifndef ALL_H
 #define ALL_H
 
+/**
+ * Berisi semua header file yang dibutuhkan oleh game.
+ * Termasuk header yang dibuat oleh masing-masing anggota kelompok
+ * dan Define semua konstanta yang digunakan dalam game.
+ */
+
+ // ==============================
+ //      C STANDARD LIBRARY
+ // ==============================
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <raylib.h>
+#include <math.h>
+#include <time.h>
+
+/* ADDED/MOVED BY: ARIEF
+ *==============================*/
+ // ==============================
+ //        CUSTOM LIBRARY
+ // ==============================
 #include "arief.h"
 #include "raffi.h"
 #include "faliq.h"
