0eebf1bf328ab96432400eafc9863a7bb349b673
Raffi <raffifauzihermawan009@gmail.com>
2025-03-16 16:50:39 +0700

    Update raffi.h

--- include/raffi.h
+++ include/raffi.h
@@ -1,20 +1,26 @@
 #ifndef RAFFI_H
 #define RAFFI_H
-#include "arief.h"
+#include "defines.h"
 
 
 //Inisialisasi nilai database
 void initializeDb(HiScore scores[]);
 
-//
-void playerInitialization(Game *game);
-
-//Menambahkan score player
-void playerScore(Game *game, HiScore scores[]);
-
-
 //Mencari nilai terbesar antara skor player dan HiScore
-int getMax(HiScore scores[], Game game);
+int getMax(HiScore scores[], Game* game, GameResources* resources);
 
+//Mengambil data dari db
+void loadHiScores(HiScore scores[]);
 
+//Menyimpan data ke db
+void saveHiScores(HiScore scores[]);
+
+//Menyimpan perubahan
+void updateHighScore(Game* game, GameResources* resources);
+
+//Aturan skor
+void addScore(Game* game, int row);
+
+//Getter score player
+long long int playerScore(Game* game);
 #endif
