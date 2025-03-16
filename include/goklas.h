616047d9cdbbaf7944d88f786c638e8f9d908bbc
riefproject <riefedu@gmail.com>
2025-03-17 03:26:46 +0700

    cherry-pick preparation

--- include/goklas.h
+++ include/goklas.h
@@ -1,23 +1,11 @@
 #ifndef GOKLAS_H
 #define GOKLAS_H
-
-#include <raylib.h>
-#include <stdlib.h>
-
-#define MAX_BULLETS 100
-#define RELOAD_DURATION 2.0 
-
-typedef struct {
-    Vector2 position;
-    int direction;
-    bool active;
-} Bullets;
-
-
+#include "defines.h"
+// 
 void InitBullets(Bullets bullets[]);
-void ShootBullets(Bullets bullets[],Vector2 playerpos, int *BulletCount, bool *CanShoot, int direction);
+void ShootBullets(Bullets bullets[], Vector2 playerpos, int* BulletCount, bool* CanShoot, int direction);
 void MoveBullets(Bullets bullets[]);
 void DrawBullets(Bullets bullets[]);
-void ReloadBullets(Bullets bullets[], int *BulletCount, bool *CanShoot);
+void ReloadBullets(Bullets bullets[], int* BulletCount, bool* CanShoot);
 
 #endif
