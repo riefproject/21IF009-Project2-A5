7b0e22a5ef232d15d77130bb923f1d7deb39f1d1
Raffi <raffifauzihermawan009@gmail.com>
2025-05-18 23:44:43 +0700

    Mengubah Array HiScore Menjadi Linked List

--- library/raffi.c
+++ library/raffi.c
@@ -1,114 +1,3 @@
-<<<<<<< HEAD
-#include "defines.h"
-#include "all.h"
-
-typedef enum {
-    Super_EZ,
-    Easy,
-    Beginner,
-    Medium,
-    Hard,
-    Super_Hard,
-    Expert,
-    Master,
-    Legend,
-    God,
-    Endless,
-}Level;
-
-const char* levelNames[] = {
-    "Super EZ",
-    "Easy",
-    "Beginner",
-    "Medium",
-    "Hard",
-    "Super Hard",
-    "Expert",
-    "Master",
-    "Legend",
-    "God",
-    "Endless",
-};
-
-void initializeDb(HiScore scores[]) {
-    FILE* file = fopen("db/hiscores.dat", "w");
-    if (!file) {
-        printf("Database belum dibuat!\n");
-        return;
-    }
-    for (int i = 0; i < MAX_LEVELS; i++) {
-        sprintf(scores[i].mode, "%s", levelNames[i]);
-        scores[i].score = 0;
-        fprintf(file, "%s,%lld\n", scores[i].mode, scores[i].score);
-    }
-    fclose(file);
-}
-
-void loadHiScores(HiScore scores[]) {
-    FILE* file = fopen("db/hiscores.dat", "r");
-    if (!file) {
-        printf("File tidak ditemukan! Menggunakan default skor 0.\n");
-        initializeDb(scores);
-        return;
-    }
-
-    char mode[20];
-    long long int score;
-    int index = 0;
-
-    while (fscanf(file, "%[^,],%lld\n", mode, &score) != EOF && index < MAX_LEVELS) {
-        strcpy(scores[index].mode, mode);
-        scores[index].score = score;
-        index++;
-    }
-
-    fclose(file);
-}
-
-void saveHiScores(HiScore scores[]) {
-    FILE* file = fopen("db/hiscores.dat", "w");
-    if (!file) {
-        printf("Gagal menyimpan skor!\n");
-        return;
-    }
-
-    for (int i = 0; i < MAX_LEVELS; i++) {
-        fprintf(file, "%s,%lld\n", scores[i].mode, scores[i].score);
-    }
-
-    fclose(file);
-}
-
-void updateHighScore(Game* game, GameResources* resources) {
-    if (!game) return;
-
-    HiScore scores[MAX_LEVELS];
-    loadHiScores(scores);
-    scores[resources->gameLevel].score = getMax(scores, game, resources);
-    saveHiScores(scores);
-}
-
-void addScore(Game* game, int row) {
-    int basePoints = 50;
-    int rowMultiplier = row + 1; // Semakin bawah multiplier semakin besar
-
-    game->score += basePoints * rowMultiplier;
-}
-
-long long int playerScore(Game* game) {
-    return game->score;
-}
-
-int getMax(HiScore scores[], Game* game, GameResources* resources) {
-    int level = resources->gameLevel - 1;
-    if (scores[level].score < game->score) {
-        return game->score;
-    }
-    else {
-        return scores[level].score;
-    }
-}
-=======
 #include "defines.h"
 #include "all.h"
 
@@ -140,49 +29,58 @@ const char* levelNames[] = {
     "Progressive",
 };
 
-void initializeDb(HiScore scores[]) {
+SingleLinkedList *initializeDb() {
     FILE* file = fopen("db/hiscores.dat", "w");
     if (!file) {
         printf("Database belum dibuat!\n");
+        return;
     }
+    SingleLinkedList *list = createSingleLinkedList();
+
     for (int i = 0; i < MAX_LEVELS; i++) {
-        sprintf(scores[i].mode, "%s", levelNames[i]);
-        scores[i].score = 0;
-        fprintf(file, "%s,%lld\n", scores[i].mode, scores[i].score);
+        HiScore *score = (HiScore*)malloc(sizeof(HiScore));
+        strcpy(score->mode, levelNames[i]);
+        score->score = 0;
+        SLL_insertBack(list, score);
+        fprintf(file, "%s,%lld\n", score->mode, score->score);
     }
     fclose(file);
+    return list;
 }
 
-void loadHiScores(HiScore scores[]) {
+SingleLinkedList *loadHiScores() {
     FILE* file = fopen("db/hiscores.dat", "r");
     if (!file) {
         printf("File tidak ditemukan! Menggunakan default skor 0.\n");
-        initializeDb(scores);
-        return;
+        return initializeDb();
     }
 
+    SingleLinkedList *list = createSingleLinkedList();
     char mode[20];
     long long int score;
-    int index = 0;
 
-    while (fscanf(file, "%[^,],%lld\n", mode, &score) != EOF && index < MAX_LEVELS) {
-        strcpy(scores[index].mode, mode);
-        scores[index].score = score;
-        index++;
+    while (fscanf(file, "%[^,],%lld\n", mode, &score) != EOF) {
+        HiScore *score = (HiScore*)malloc(sizeof(HiScore));
+        strcpy(score->mode, mode);
+        score->score = score;
+        SLL_insertBack(list, score);
     }
 
     fclose(file);
 }
 
-void saveHiScores(HiScore scores[]) {
+void saveHiScores(SingleLinkedList *list) {
     FILE* file = fopen("db/hiscores.dat", "w");
     if (!file) {
         printf("Gagal menyimpan skor!\n");
         return;
     }
 
-    for (int i = 0; i < MAX_LEVELS; i++) {
-        fprintf(file, "%s,%lld\n", scores[i].mode, scores[i].score);
+    SLLNode* temp = SLL_getFront(list);
+    while(temp){
+        HiScore* score = (HiScore*)SLL_getNodeData(temp);
+        fprintf(file, "%s,%lld\n", score->mode, score->score);
+        temp = SLL_getNextNode(temp);
     }
 
     fclose(file);
@@ -191,10 +89,21 @@ void saveHiScores(HiScore scores[]) {
 void updateHighScore(Game* game, GameResources* resources) {
     if (!game) return;
 
-    HiScore scores[MAX_LEVELS];
-    loadHiScores(scores);
-    scores[resources->gameLevel].score = getMax(scores, game, resources);
-    saveHiScores(scores);
+    SingleLinkedList *list = loadHiScores();
+    SLLNode *temp = SLL_getFront(list);
+    const char *mode = levelNames[resources->gameLevel];
+
+    while(temp){
+        HiScore* score = (HiScore*)SLL_getNodeData(temp);
+        if(strcmp(score->mode,mode) == 0){
+            if(score->score < game->score){
+                score->score = game->score;
+            }
+            break;
+        }
+        temp = SLL_getNextNode(temp);
+    }
+    saveHiScores(list);
 }
 
 void addScore(Game* game, int row) {
@@ -208,17 +117,7 @@ long long int playerScore(Game* game) {
     return game->score;
 }
 
-long long int getMax(HiScore scores[], Game* game, GameResources* resources) {
-    int level = resources->gameLevel;
-    if (scores[level].score < game->score) {
-        return game->score;
-    }
-    else {
-        return scores[level].score;
-    }
-}
 
 char* gameMode(GameResources* resources) {
     return levelNames[resources->gameLevel];
 }
->>>>>>> cbf3f8c5a6734aae2f3fe2c040e10d9982a3f4de
